9048e8d9ec1ad6118b3640d976748ff22804e694
aryamangala <aryamangala@Aryamans-MacBook-Air.local>
2021-04-08 15:22:46 -0700

    functions

--- in_class_code.R
+++ in_class_code.R
@@ -4,3 +4,15 @@ lightHour <- lightMinute*60
 lightDay <- lightHour*24
 lightYear <- lightDay*365.2422
 cat("length of light year:", lightYear,"\n")
+
+add <- function(x,y){
+  x+y
+  cat("x =", x, "y =", y, "\n")
+}
+
+lightyear <- function(ly) {
+  dist = ly*lightYear
+  cat("distance in kilometers=", dist)
+}
+
+lightyear(55000000)
